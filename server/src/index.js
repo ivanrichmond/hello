df1bbec6038659d32905fc7e418fd7df758545d5
ivanrichmond <irichmond@yahoo.com>
2023-01-18 17:29:30 -0800

    initial setup of server and client (#1)
    * add config file
    
    * remove old server/config.json, since config is now at project root
    
    * remove old server/index.js, since that belongs in server/src/
    
    * add placeholder db.js
    
    * add functional placeholder index.js for server-side
    
    * refactor: actually, decided to decouple client and server afterall
    
    * added server-side as proxy to client package.json
    
    * added client-side fetch of server-side api endpoint

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+//@flow
+// External Modules
+import express from 'express';
+// import bodyParser from 'body-parser';
+
+// Internal Modules
+import * as config from '../config.json';
+import dbConnect from './db.js';
+
+// Application Variables
+const app = express();
+// var jsonParser = bodyParser.json();
+const port = config.serverPort;
+
+// Database
+dbConnect(); // placeholder for DB connection.
+
+// Placeholder for initial endpoint.
+app.get('/api', (req, res) => {
+    res.send({ message: "Successfully connected to server." });
+})
+
+// Listen at port.
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
