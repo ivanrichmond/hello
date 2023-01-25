ce087eb7911097f888ab46700188015d4bc797f3
Ivan Richmond <irichmond@yahoo.com>
2023-01-24 17:54:27 -0800

    add sort backend with initial bubble sort for scalars as only option

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,62 @@
 //@flow
+
+/*
+ * Defines the endpoints of the server-side.
+ * 
+ * Requestuest Format:
+ * ---------------
+ * Some endpoints do not requestuire requestuests.
+ * Those that do should be as: 
+ * { payload: whatever the main value is that is needed }
+ * 
+ * Response Format:
+ * ----------------
+ * { message: message string to return to client, payload: the main result, such as data returned }
+ */
+
 // External Modules
 import express from 'express';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 // Internal Modules
 import * as config from '../config.json';
-import { count } from './helpers/count.js';
+import { bubbleSort } from './helpers/sort';
+import { count } from './helpers/count';
 import dbConnect from './db.js';
 
 // Application Variables
 const app = express();
-// var jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 const port = config.serverPort;
 
 // Database
 dbConnect(); // placeholder for DB connection.
 
-// Placeholder for initial endpoint.
-app.get('/api', (req, res) => {
-    res.send({ message: "Successfully connected to server." });
-})
 
 // Placeholder for initial endpoint.
-app.get('/count', (req, res) => {
+app.get('/api', (request, response) => {
+    response.send({ message: "Successfully connected to server.", payload: null });
+})
+
+// Counts how many times this endpoint has been hit.
+app.get('/count', (request, response) => {
     const trafficCount = count();
-    res.send({ message: `This endpoint has been hit ${trafficCount.countUp()} times since the server started.` });
+    const counter = trafficCount.countUp();
+    response.send({
+        message: `This endpoint has been hit ${counter} times since the server started.`,
+        payload: counter
+    });
+})
+
+// Uses bubble sort.
+app.post('/sort', jsonParser, (request, response) => {
+    console.debug('request.body', request.body);
+    const array = request.body?.payload;
+    const arraySorted = bubbleSort(array);
+    response.send({ 
+        message: `Your array has been sorted.`, 
+        payload: arraySorted 
+    });
 })
 
 // Listen at port.
