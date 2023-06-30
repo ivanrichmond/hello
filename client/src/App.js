5c4fa0142f524f85177bc7180f0293ca74c65b7f
Ivan Richmond <irichmond@yahoo.com>
2023-06-30 14:54:58 -0700

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,63 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [data, setData] = React.useState(null);
+function Navbar() {
+  return (
+    <div>
+      TODO: NAVBAR GOES HERE
+    </div>
+  )
+}
 
+function Home() {
   React.useEffect(() => {
     fetch("/api")
-      .then(res => res.json())
+      .then(res => {
+        if(res.ok){
+          return res.json()
+        } else {
+          console.error("Data could not be retrieved from /api")
+          return null
+        }
+      })
       .then(data => {
-        if(!data.message){ console.error("data malformed: no message property")}
-        setData(data?.message)
+        if(data){
+          if(data?.message){ console.error("Data malformed: no message property")}
+          setData(data?.message)
+        }
       })
   }, []);
-
+  
+  const [data, setData] = React.useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Hello suite.</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Welcome to the Hello suite.</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{!data ? "Loading..." : data}</p>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <Home/>
+          }>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
