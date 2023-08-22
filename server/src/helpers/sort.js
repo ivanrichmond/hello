90ddf270ae41ea0b64a102818db8a05cf3bd443c
ivanrichmond <irichmond@yahoo.com>
2023-08-22 14:13:39 -0700

    move array-helpers to experiments (#8)
    * add sqllite3 package
    
    * add binary search array helper
    
    * some array problems (#7)

--- server/src/helpers/sort.js
+++ server/src/helpers/sort.js
@@ -1,4 +1,41 @@
 /**
+ * Perform a binary search to find an element in a sorted array.
+ * @param array - flat sorted array of int | string
+ * @param int | string - element to find
+ * @return int - index of found element
+ */
+export const binarySearch = (array: Array < number | string >, search: number | string ) => {
+    let index = -1;
+    let found = false;
+
+    const getMidpoint = (l,r) => {
+        const midPoint = Math.floor((l + r) / 2)
+        return midPoint
+    }
+
+    let i = getMidpoint(0,array.length - 1)
+
+    while(!found && i >= 0 && i < array.length){
+        const element = array[i]
+        if(element === search){
+            index = i
+            found = true;
+        } else if(element > search) {
+            // element is too far right, so narrow to the left
+            i = getMidpoint(0,i)
+        } else if(element < search){
+            // element is too far left, so narrow to the right
+            i = getMidpoint(i + 1,array.length - 1)
+        } else {
+            // Something went wrong.
+            throw new Error("binarySearch: the array was not sorted or something else went wrong.")
+        }
+    }
+
+    return index;
+}
+
+/**
  * Bubble sort for scalars.  Will not work for objects, as it uses simple GT comparison.
  * @param Array array of scalars to sort.
  * @return Array sorted array.
