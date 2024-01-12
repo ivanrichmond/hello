3fa6ebb72a5959695bad03fc1b8c44345901a371
Ivan Richmond <irichmond@yahoo.com>
2024-01-11 18:09:44 -0800

    remove userReducer from Redux store, because now using apiSlice

--- client/src/data/store.js
+++ client/src/data/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { apiSlice } from '../features/api/apiSlice'
-import usersReducer from '../data/usersSlice'
 
 export default configureStore({
   reducer: {
-      users: usersReducer,
       [apiSlice.reducerPath]: apiSlice.reducer
   }
 })
