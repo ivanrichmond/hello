50357b23a2a1d77140d65b7fb517c970e4adff20
Ivan Richmond <irichmond@yahoo.com>
2024-01-29 15:51:31 -0800

    fix test data

--- client/src/routes/editUser.jsx
+++ client/src/routes/editUser.jsx
@@ -8,6 +8,7 @@ import AppButton from '../styleLibrary/AppButton'
 import AppGrid from '../styleLibrary/AppGrid'
 import AppInput from '../styleLibrary/AppInput'
 import AppLoader from '../styleLibrary/AppLoader';
+import Notice from '../components/Notice'
 import { AuthContext } from '../contexts/AuthProvider'
 import { useGetUserQuery } from '../features/api/apiSlice';
 import { useNotice } from '../contexts/NoticeProvider'
@@ -16,7 +17,7 @@ import { useEffect } from 'react';
 export default function EditUser() {
   const navigate = useNavigate();
   const { userId } = useParams()
-  const { createNotice } = useNotice()
+  const { createNotice, deleteNotice, notice } = useNotice()
   if(!userId) {
     createNotice(`Sorry, something went wrong.`, 'error')
     navigate(-1)
@@ -30,9 +31,11 @@ export default function EditUser() {
   const {
     currentUser,
     currentUserError,
+    deleteUser,
     isCurrentUserError,
     isCurrentUserLoading,
     isLoggedIn,
+    isUniqueUsername,
     updateCurrentUser,
     updateUser,
   } = useContext(AuthContext)
@@ -52,7 +55,14 @@ export default function EditUser() {
   // This is only used as an adjunct to action, in order to update AuthContext.
   const handleSubmit = (e) => {
     e.preventDefault()
-    updateUser(newUser)
+    if(isUniqueUsername(newUser)){
+      updateUser(newUser)
+    } else {
+      deleteUser(newUser?.id)
+      createNotice(`${newUser?.username} is already taken.`, 'error')
+      return
+    }
+
     if(isLoggedIn){
       if(isYou){
         // If this is the user's own record, send them back home.
@@ -72,89 +82,98 @@ export default function EditUser() {
     (<AppLoader />)
     : 
     (
-    <form method="post" id="user-form" onSubmit={e => handleSubmit(e)}>
-      <h1>{heading}</h1>
-      <AppGrid>
-        <AppGrid.Row>
-          <AppGrid.Column>
-            <label htmlFor={'name'}>Name: </label>
-          </AppGrid.Column>
-          
-          <AppGrid.Column>
-            <AppInput
-              aria-label="Name"
-              defaultValue={user?.name || ''}
-              id="name"
-              name="name"
-              onChange = {
-                e => setNewUser({...newUser, name: e.target.value})
-              }
-              placeholder="Name"
-              type="text"
-              />
-          </AppGrid.Column>
-        </AppGrid.Row>
-        <AppGrid.Row>
-          <AppGrid.Column>
-            <label htmlFor={'username'}>Username: </label>
-          </AppGrid.Column>
-          
-          <AppGrid.Column>
-            <AppInput
-              aria-label="username"
-              defaultValue={user?.username || ''}
-              id="username"
-              name="username"
-              onChange = {
-                e => setNewUser({...newUser, username: e.target.value})
-              }
-              placeholder="username"
-              type="text"
-              />
-          </AppGrid.Column>
-        </AppGrid.Row>
-        <AppGrid.Row>
-          <AppGrid.Column>
-            <label htmlFor={'password'}>Password: </label>
-          </AppGrid.Column>
-          
-          <AppGrid.Column>
-            <AppInput
-              aria-label="password"
-              defaultValue={user?.password || ''}
-              id="password"
-              name="password"
-              onChange = {
-                e => setNewUser({...newUser, password: e.target.value})
-              }
-              placeholder="password"
-              type="password"
-            /> 
-          </AppGrid.Column>
-        </AppGrid.Row>
-        <AppGrid.Row>
-          <AppGrid.Column>
-            <AppButton
-            color = "green"
-            type="submit"
-            >
-              Save
-            </AppButton>
-          </AppGrid.Column>
+    <>
+      {notice && 
+        <Notice
+        close = {deleteNotice}
+        message = {notice?.message}
+        type = {notice?.type}
+        />
+      }
+      <form method="post" id="user-form" onSubmit={e => handleSubmit(e)}>
+        <h1>{heading}</h1>
+        <AppGrid>
+          <AppGrid.Row>
+            <AppGrid.Column>
+              <label htmlFor={'name'}>Name: </label>
+            </AppGrid.Column>
+            
+            <AppGrid.Column>
+              <AppInput
+                aria-label="Name"
+                defaultValue={user?.name || ''}
+                id="name"
+                name="name"
+                onChange = {
+                  e => setNewUser({...newUser, name: e.target.value})
+                }
+                placeholder="Name"
+                type="text"
+                />
+            </AppGrid.Column>
+          </AppGrid.Row>
+          <AppGrid.Row>
+            <AppGrid.Column>
+              <label htmlFor={'username'}>Username: </label>
+            </AppGrid.Column>
+            
+            <AppGrid.Column>
+              <AppInput
+                aria-label="username"
+                defaultValue={user?.username || ''}
+                id="username"
+                name="username"
+                onChange = {
+                  e => setNewUser({...newUser, username: e.target.value})
+                }
+                placeholder="username"
+                type="text"
+                />
+            </AppGrid.Column>
+          </AppGrid.Row>
+          <AppGrid.Row>
+            <AppGrid.Column>
+              <label htmlFor={'password'}>Password: </label>
+            </AppGrid.Column>
+            
+            <AppGrid.Column>
+              <AppInput
+                aria-label="password"
+                defaultValue={user?.password || ''}
+                id="password"
+                name="password"
+                onChange = {
+                  e => setNewUser({...newUser, password: e.target.value})
+                }
+                placeholder="password"
+                type="password"
+              /> 
+            </AppGrid.Column>
+          </AppGrid.Row>
+          <AppGrid.Row>
+            <AppGrid.Column>
+              <AppButton
+              color = "green"
+              type="submit"
+              >
+                Save
+              </AppButton>
+            </AppGrid.Column>
 
-          <AppGrid.Column>
-            <AppButton 
-            color = "red"
-            type="button"
-            onClick={() => {
-                navigate(-1)
-            }}
-            >
-              Cancel
-            </AppButton>
-          </AppGrid.Column>
-        </AppGrid.Row>
-      </AppGrid>
-    </form>
+            <AppGrid.Column>
+              <AppButton 
+              color = "red"
+              type="button"
+              onClick={() => {
+                  navigate(-1)
+              }}
+              >
+                Cancel
+              </AppButton>
+            </AppGrid.Column>
+          </AppGrid.Row>
+        </AppGrid>
+      </form>
+    </>
   );
 }
