b82d269ddbc05a515d5d1191bd8e202fa6964c27
Ivan Richmond <irichmond@yahoo.com>
2024-01-29 13:20:28 -0800

    update unit tests to match Redux changes

--- client/src/routes/login.test.js
+++ client/src/routes/login.test.js
@@ -1,11 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react'
-import { Provider as ReduxProvider } from 'react-redux'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
+
 import Login from './login';
 import { AuthProvider } from '../contexts/AuthProvider.jsx'
 import { NoticeProvider } from '../contexts/NoticeProvider.jsx'
-import store from '../data/store'
 import { apiSlice } from '../features/api/apiSlice'
 
 const mockedUsedNavigate = jest.fn();
@@ -17,15 +16,13 @@ jest.mock('react-router-dom', () => ({
 
 test('renders Login', () => {
   render(
-      <ReduxProvider store = {store}>
-        <ApiProvider api={apiSlice}>
-          <NoticeProvider>
-              <AuthProvider>
-                <Login />
-              </AuthProvider>
-          </NoticeProvider>
-        </ApiProvider>
-      </ReduxProvider>
+    <ApiProvider api={apiSlice}>
+      <NoticeProvider>
+        <AuthProvider>
+            <Login />
+        </AuthProvider>
+      </NoticeProvider>
+    </ApiProvider> 
   );
   const element = screen.getByText(/Login/i);
   expect(element).toBeInTheDocument();
--- client/src/routes/root.test.js
+++ client/src/routes/root.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react'
-import { Provider as ReduxProvider } from 'react-redux'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
+import { Segment } from 'semantic-ui-react'
+
 import { apiSlice } from '../features/api/apiSlice';
 import Root from './root';
 import { AuthProvider } from '../contexts/AuthProvider.jsx'
 import { NoticeProvider } from '../contexts/NoticeProvider.jsx'
-import store from '../data/store'
 
 const mockedUsedNavigate = jest.fn();
 
@@ -17,15 +17,15 @@ jest.mock('react-router-dom', () => ({
 
 test('renders hello!', () => {
   render(
-    <ReduxProvider store = {store}>
-      <ApiProvider api={apiSlice}>
-        <NoticeProvider>
-            <AuthProvider>
-              <Root />
-            </AuthProvider>
-        </NoticeProvider>
-      </ApiProvider>
-    </ReduxProvider>
+    <ApiProvider api={apiSlice}>
+      <NoticeProvider>
+        <AuthProvider>
+          <Segment className='MainSegment' padded>
+            <Root test />
+          </Segment>
+        </AuthProvider>
+      </NoticeProvider>
+    </ApiProvider> 
   );
   const element = screen.getByText(/hello!/i);
   expect(element).toBeInTheDocument();
