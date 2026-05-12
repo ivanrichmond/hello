17349fc17247dea24ea3421c0c77863a48ac9389
Ivan Richmond <irichmond@yahoo.com>
2026-05-12 15:16:56 -0700

    fix Flow errors in data structures helper and unit tests

--- server/src/helpers/dataStructures.js
+++ server/src/helpers/dataStructures.js
@@ -2,10 +2,21 @@
 
 // --- Linked List --- //
 export class LinkedListNode {
-    data: any; // any is appropriate, because we don't know what all will be linked
-    next: any;
-    constructor(data: any){
+    data: string; 
+    next: LinkedListNode | null;
+    constructor(data: string){
         this.data = data
         this.next = null
+    }
+
+    toString(): string {
+        let result: string = ''
+        let temp = this
+        result += this.data
+        while (temp !== null && temp !== undefined && temp.next) {
+            result += temp.next.data
+            temp = temp?.next
+        }
+        return result
     }
 }
