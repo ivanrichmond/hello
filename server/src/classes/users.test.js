2271db35af3ae854b138219698e8529ffda73dbd
Ivan Richmond <irichmond@yahoo.com>
2026-05-11 18:06:48 -0700

    update User class tests to get full coverage

--- server/src/classes/users.test.js
+++ server/src/classes/users.test.js
@@ -2,12 +2,20 @@ import {
     User,
 } from './users.js'
 
-const user = new User('Dude', 'dude', 'abides')
 
-test('can create a User object',() => {
-    // name = null, username = null, password = null
-    expect(user.name).toEqual('Dude')
-    expect(user.username).toEqual('dude')
-    expect(user.password).toEqual('abides')
+describe('new User', () => {
+    test('can create a User object',() => {
+        const user = new User('Dude', 'dude', 'abides')
+        expect(user.name).toEqual('Dude')
+        expect(user.username).toEqual('dude')
+        expect(user.password).toEqual('abides')
+    })
+    
+    test('can create a blank User object',() => {
+        const user = new User()
+        expect(user.name).toEqual('')
+        expect(user.username).toEqual('')
+        expect(user.password).toEqual('')
+    })
 })
 
