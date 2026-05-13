3f737a939a1cf7a764740a3919a121458bf17bcd
Ivan Richmond <irichmond@yahoo.com>
2026-05-13 16:28:52 -0700

    add TreeNode class to data structures helper

--- server/src/helpers/dataStructures.js
+++ server/src/helpers/dataStructures.js
@@ -1,8 +1,10 @@
 //@flow
 
 // --- Linked List --- //
+import isNull from "lodash/fp/isNull";
+
 export class LinkedListNode {
-    data: any; 
+    data: any; // Appropriate typing given that we don't know what data will be linked
     next: LinkedListNode | null;
     constructor(data: any){
         this.data = data
@@ -18,5 +20,16 @@ export class LinkedListNode {
             temp = temp?.next
         }
         return result
+    }
+}
+
+export class TreeNode {
+    data: any;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(data: any){
+        this.data = data
+        this.left = null
+        this.right = null
     }
 }
